bcf2099da60095209a0415732085af97075be13e
user-name <zinovev.evgenii@mail.ru>
2023-06-07 17:49:55 +0300

    finalerror

---
+++ acme_project/birthday/forms.py
@@ -0,0 +1,13 @@
+from django import forms
+
+from .models import Birthday
+
+class BirthdayForm(forms.ModelForm):
+    # Удаляем все описания полей.
+
+    # Все настройки задаём в подклассе Meta.
+    class Meta:
+        # Указываем модель, на основе которой должна строиться форма.
+        model = Birthday
+        # Указываем, что надо отобразить все поля.
+        fields = '__all__' 
